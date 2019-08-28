042bedb40ab124c432dfc05c4a0f4322f8c99e76
VietThang <vietthangtik15@gmail.com>
2019-08-28 14:27:19 +0700

    change path

--- face_detection/accuracy_evaluation/predict.py
+++ face_detection/accuracy_evaluation/predict.py
@@ -256,9 +256,10 @@ def run_prediction_folder():
         # im = cv2.imread(os.path.join(debug_folder, file_name))
 
         bboxes = my_predictor.predict(im, resize_scale=1, score_threshold=0.3, top_k=10000, NMS_threshold=0.3, NMS_flag=True, skip_scale_branch_list=[])
-        for bbox in bboxes:
-            print(bbox)
-            cv2.rectangle(im, (bbox[0], bbox[1]), (bbox[2], bbox[3]), (0, 255, 0), 2)
+        for bboxs in bboxes:
+            # print(bbox)
+            for bbox in bboxs:
+                cv2.rectangle(im, (bbox[0], bbox[1]), (bbox[2], bbox[3]), (0, 255, 0), 2)
 
         if max(im.shape[:2]) > 1600:
             scale = 1600/max(im.shape[:2])
