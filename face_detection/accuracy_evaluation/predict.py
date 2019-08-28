928e18f8d355d64f9cb24ae57100cccab281e4bf
VietThang <vietthangtik15@gmail.com>
2019-08-28 21:35:38 +0700

    fix detect faces

--- face_detection/accuracy_evaluation/predict.py
+++ face_detection/accuracy_evaluation/predict.py
@@ -219,9 +219,11 @@ class Predict(object):
             for i in range(final_bboxes.shape[0]):
                 final_bboxes_.append((final_bboxes[i, 0], final_bboxes[i, 1], final_bboxes[i, 2], final_bboxes[i, 3], final_bboxes[i, 4]))
 
-            return final_bboxes_, infer_time
+            # return final_bboxes_, infer_time
+            return final_bboxes_
         else:
-            return bbox_collection_numpy, infer_time
+            # return bbox_collection_numpy, infer_time
+            return bbox_collection_numpy
 
 
 def run_prediction_folder():
@@ -256,17 +258,16 @@ def run_prediction_folder():
         # im = cv2.imread(os.path.join(debug_folder, file_name))
 
         bboxes = my_predictor.predict(im, resize_scale=1, score_threshold=0.3, top_k=10000, NMS_threshold=0.3, NMS_flag=True, skip_scale_branch_list=[])
-        print(len(bboxes))
-        # for bboxs in bboxes:
-        #     # print(len(bboxs))
-        #     # print('{}\n'.format(bboxes))
-        #     for bbox in bboxs:
-        #         # print(len(bbox))
-        #         cv2.rectangle(im, (int(bbox[0]), int(bbox[1])), (int(bbox[2]), int(bbox[3])), (0, 255, 0), 2)
+        # print(len(bboxes))
+        for bboxs in bboxes:
+            print('{}\n'.format(bboxs))
+            # for bbox in bboxs:
+                # print(len(bbox))
+            cv2.rectangle(im, (int(bboxs[0]), int(bboxs[1])), (int(bboxs[2]), int(bboxs[3])), (0, 255, 0), 2)
 
-        # if max(im.shape[:2]) > 1600:
-        #     scale = 1600/max(im.shape[:2])
-        #     im = cv2.resize(im, (0, 0), fx=scale, fy=scale)
+        if max(im.shape[:2]) > 1600:
+            scale = 1600/max(im.shape[:2])
+            im = cv2.resize(im, (0, 0), fx=scale, fy=scale)
         # cv2.imshow('im', im)
         # cv2.waitKey()
         out.write(im)
