045d80f0ea97561a3832c9c77b88d9c6730e2cd0
VietThang <vietthangtik15@gmail.com>
2019-08-28 21:33:02 +0700

    fix detect faces

--- face_detection/accuracy_evaluation/predict.py
+++ face_detection/accuracy_evaluation/predict.py
@@ -257,16 +257,16 @@ def run_prediction_folder():
 
         bboxes = my_predictor.predict(im, resize_scale=1, score_threshold=0.3, top_k=10000, NMS_threshold=0.3, NMS_flag=True, skip_scale_branch_list=[])
         print(len(bboxes))
-        for bboxs in bboxes:
-            # print(len(bboxs))
-            # print('{}\n'.format(bboxes))
-            # for bbox in bboxs:
-                # print(len(bbox))
-                # cv2.rectangle(im, (int(bbox[0]), int(bbox[1])), (int(bbox[2]), int(bbox[3])), (0, 255, 0), 2)
+        # for bboxs in bboxes:
+        #     # print(len(bboxs))
+        #     # print('{}\n'.format(bboxes))
+        #     for bbox in bboxs:
+        #         # print(len(bbox))
+        #         cv2.rectangle(im, (int(bbox[0]), int(bbox[1])), (int(bbox[2]), int(bbox[3])), (0, 255, 0), 2)
 
-        if max(im.shape[:2]) > 1600:
-            scale = 1600/max(im.shape[:2])
-            im = cv2.resize(im, (0, 0), fx=scale, fy=scale)
+        # if max(im.shape[:2]) > 1600:
+        #     scale = 1600/max(im.shape[:2])
+        #     im = cv2.resize(im, (0, 0), fx=scale, fy=scale)
         # cv2.imshow('im', im)
         # cv2.waitKey()
         out.write(im)
