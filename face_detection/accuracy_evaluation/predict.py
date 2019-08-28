a614d9bd592c6ff3bbf93a3a35f1301f284451bd
VietThang <vietthangtik15@gmail.com>
2019-08-28 21:26:12 +0700

    fix detect faces

--- face_detection/accuracy_evaluation/predict.py
+++ face_detection/accuracy_evaluation/predict.py
@@ -251,16 +251,17 @@ def run_prediction_folder():
     out = cv2.VideoWriter('/content/output.avi',fourcc, 20.0, (1920,1080))
     while True:
         ret, im = cap.read()
-        print(im.shape)
+        # print(im.shape)
         assert ret==True
         # im = cv2.imread(os.path.join(debug_folder, file_name))
 
         bboxes = my_predictor.predict(im, resize_scale=1, score_threshold=0.3, top_k=10000, NMS_threshold=0.3, NMS_flag=True, skip_scale_branch_list=[])
         for bboxs in bboxes:
             # print(len(bboxs))
-            for bbox in bboxs:
-                # print(len(bbox))
-                cv2.rectangle(im, (int(bbox[0]), int(bbox[1])), (int(bbox[2]), int(bbox[3])), (0, 255, 0), 2)
+            print('{}\n'.format(bboxes))
+            # for bbox in bboxs:
+            #     # print(len(bbox))
+            #     cv2.rectangle(im, (int(bbox[0]), int(bbox[1])), (int(bbox[2]), int(bbox[3])), (0, 255, 0), 2)
 
         if max(im.shape[:2]) > 1600:
             scale = 1600/max(im.shape[:2])
